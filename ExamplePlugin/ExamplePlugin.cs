eba7ac6ce927d454aaee19b66f5378d8d7ad6c06
BTP <3503922521@qq.com>
2023-03-23 15:30:26 +0800

    添加项目文件。

---
+++ ExamplePlugin/ExamplePlugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using R2API;
+using R2API.Utils;
+
+namespace ExamplePlugin {
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [R2APISubmoduleDependency(nameof(LanguageAPI))]
+    public class ExamplePlugin : BaseUnityPlugin {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "BTP";
+        public const string PluginName = "BTP_localization";
+        public const string PluginVersion = "1.0.0";
+        public void Awake() {
+            AddTokens();
+        }
+        private void AddTokens() {
+            //LanguageAPI.Add("COF_REDMIST_BODY_NAME", "红雾");
+            //LanguageAPI.Add("AbyssalMedkit", "阿比斯弹夹");
+        }
+    }
+}
